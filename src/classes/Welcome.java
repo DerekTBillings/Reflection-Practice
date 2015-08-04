35bbb1a543eaec5bc77efc1757e0627b01caa6a7
DerekTBillings <aikimavirtue@gmail.com>
2015-08-04 05:49:20 -0500

    Revert "Main file of this application"
    This reverts commit 30341376bb70092f15276ece5475517406434e48.

--- src/classes/Welcome.java
+++
@@ -1,9 +0,0 @@
-package classes;
-
-public class Welcome extends Notification {
-
-	public void welcome(String name, StringBuffer location) {
-		notify("Welcome "+name+" to "+location.reverse());
-	}
-	
-}
